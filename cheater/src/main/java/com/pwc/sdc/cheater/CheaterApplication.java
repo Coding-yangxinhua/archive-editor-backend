e493b8dd29d4fe2643e703206e3adab9809aa306
Xinhua X Yang <xinhua.x.yang@cn.pwc.com>
2023-08-16 13:25:41 +0800

    基础结构

---
+++ cheater/src/main/java/com/pwc/sdc/cheater/CheaterApplication.java
@@ -0,0 +1,11 @@
+package com.pwc.sdc.cheater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheaterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CheaterApplication.class, args);
+    }
+}
