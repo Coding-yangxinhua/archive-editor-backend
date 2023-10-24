d7fae7fc387c4ea8866d43a29a8032c28240176f
Xinhua X Yang <xinhua.x.yang@cn.pwc.com>
2023-10-24 11:18:37 +0800

    feat(config): 邮件模块，发送任意主题内容邮件

---
+++ src/main/java/com/pwc/sdc/archive/controller/MailController.java
@@ -0,0 +1,28 @@
+package com.pwc.sdc.archive.controller;
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.pwc.sdc.archive.common.annotation.Auth;
+import com.pwc.sdc.archive.common.bean.ResponseEntity;
+import com.pwc.sdc.archive.common.constants.RoleConstants;
+import com.pwc.sdc.archive.common.handler.MailService;
+import com.pwc.sdc.archive.domain.AeGameItem;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@ApiOperation("邮件接口")
+@RestController
+@RequestMapping("mail")
+public class MailController {
+    @Autowired
+    private MailService mailService;
+
+    @Auth(roles = {RoleConstants.ADMIN})
+    @PostMapping("/send")
+    @ApiOperation(value = "发送任意内容邮件", httpMethod = "POST")
+    public ResponseEntity<IPage<AeGameItem>> list(String to, String subject, String text) {
+        mailService.sendMail(to, subject, text);
+        return ResponseEntity.ok();
+    }
+
+}
